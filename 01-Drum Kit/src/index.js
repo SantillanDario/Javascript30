e047bf9d7c59728748e2d2e72a816358747e0359
Dario <santillandario823@gmail.com>
2021-10-26 00:04:36 -0300

    add listener for transition end

--- 01-Drum Kit/src/index.js
+++ 01-Drum Kit/src/index.js
@@ -1,6 +1,18 @@
-window.addEventListener('keydown', (event) => {
+function playSound() {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     if (!audio) return;
+    audio.currentTime = 0; //Resetea el audio al inicio
     audio.play()
-    console.log(audio)
-});
+    key.classList.add('playing');
+
+}
+
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return; //retorna si no es transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound)
